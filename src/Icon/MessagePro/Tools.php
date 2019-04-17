f47ba4702d2c269abc31a25e28e95bc3d4f07b1c
jiaicon@163.com <jiaicon@163.com>
2019-04-17 16:55:19 +0800

    push

---
+++ src/Icon/MessagePro/Tools.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: icon
+ * Date: 2019/4/17
+ * Time: 下午4:26
+ */
+namespace Icon\MessagePro;
+
+class Tools {
+    public function __construct() {
+    }
+
+    public static function Hello() {
+        return "Hello World";
+    }
+}
